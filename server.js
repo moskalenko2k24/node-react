f880d6d3993f44f3772ddad48782e1507e351073
moskalenco_a <moskalenco.a@gmail.com>
2023-09-24 05:01:56 +0300

    Fix client routing bug

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
 app.set('port', process.env.port || 3001);
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.set('port', process.env.port || 3000);
-}
-
 app.get('/api/send', (req, res) => {
   res.send({ value: 'new value' });
 });
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.set('port', process.env.port || 3000);
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
